22c1b9bfa7c6eb148926c3ae992f961b1ab1230e
pvosper <pvosper@mac.com>
2017-01-22 09:59:59 -0800

    WIP: First test passing

--- resources/session02/homework/http_server.py
+++ resources/session02/homework/http_server.py
@@ -1,5 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+Homework
+    Take the following steps one at a time. Run the tests in
+    assignments/session02/homework between to ensure that you are getting it
+    right.
+
+    1) Complete the stub resolve_uri function so that it handles looking up
+    resources on disk using the URI returned by parse_request.
+
+    2) Make sure that if the URI does not map to a file that exists, it raises
+    an appropriate error for our server to handle.
+
+    3) Complete the response_not_found function stub so that it returns a 404
+    response.
+
+    4) Update response_ok so that it uses the values returned by resolve_uri by
+    the URI. (these have already been added to the function signature)
+    You'll plug those values into the response you generate in the way
+    required by the protocol
+
+    @maria:
+
+    When starting the homework, start by first getting just this test to pass:
+    ```python tests.py ResolveURITestCase.test_directory_resource```
+
+    You might find the test to look confusing at first. It is testing the
+    function resolve_uri (the function above this test assigns
+    call_function_under_test to this function).
+
+    It is expecting that this function is going to return two objects,
+
+    Look at the test to figure out what these objects should be.
+"""
+
 import socket
 import sys
+import os
 
 
 def response_ok(body=b"this is a pretty minimal response", mimetype=b"text/plain"):
@@ -32,10 +69,35 @@ def parse_request(request):
         raise NotImplementedError("We only accept GET")
     return uri
 
-
+# It is expecting that this function is going to return two objects,
+# Look at the test to figure out what these objects should be.
+# 1) Complete the stub resolve_uri function so that it handles looking up
+#   resources on disk using the URI returned by parse_request.
+# actual_body, actual_mimetype
 def resolve_uri(uri):
     """This method should return appropriate content and a mime type"""
-    return b"still broken", b"text/plain"
+    """Your resolve_uri function will need to accomplish the following tasks:
+
+    It should take a URI as the sole argument
+    It should map the pathname represented by the URI to a filesystem location.
+    It should have a 'home directory', and look only in that location.
+    If the URI is a directory, it should return a plain-text listing of the
+        directory contents and the mimetype text/plain.
+    If the URI is a file, it should return the contents of that file and its
+        correct mimetype.
+    If the URI does not map to a real location, it should raise an exception
+        that the server can catch to return a 404 response.
+    """
+    content = b""
+    if uri[-1] == "/":
+        path  = os.getcwd() + '//webroot' + uri
+        content = os.listdir(path)
+        content = ';'.join(content)
+            # .encode('utf-8')
+    # with open(filepath) as f:
+    #     content = f.readlines()
+    # return b"still broken", b"text/plain"
+    return content.encode('utf-8'), b"text/plain"
 
 
 def server(log_buffer=sys.stderr):
